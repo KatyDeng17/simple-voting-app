6d65b134c631eaacdc5c78ba5179f49e948604d9
katy Deng <deng2017.xq@gmail.com>
2019-02-13 15:48:45 -0800

    fetching data and display the data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express'); 
 const app = express(); 
+const fs = require('fs');
 app.get('/', (req,res)=>{
   res.sendFile(__dirname + '/index.html')
 })
+app.get('/poll', (req, res)=>{
+  fs.readFile(__dirname + '/poll.json', 'utf8', (err, data)=>{
+    res.send(data);
+  })
+})
+
+
 app.listen(3001, ()=>{
   console.log('listening on port 3001'); 
 })
