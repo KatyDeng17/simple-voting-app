13447a16ca5fcbf78a1c52ae04584a1abd39e00b
katy Deng <deng2017.xq@gmail.com>
2019-02-13 15:59:49 -0800

    adding body-parser middleware

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express'); 
 const app = express(); 
 const fs = require('fs');
+const bodyParser = require('body-parser')//adding body-parser middleware
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
 app.get('/', (req,res)=>{
   res.sendFile(__dirname + '/index.html')
 })
@@ -9,7 +15,9 @@ app.get('/poll', (req, res)=>{
     res.send(data);
   })
 })
-
+app.post('/vote/new',(req, res)=>{
+  console.log(req.body);
+})
 
 app.listen(3001, ()=>{
   console.log('listening on port 3001'); 
